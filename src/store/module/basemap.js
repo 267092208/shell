3f2825e1124ccb80de3a48411a710c0036e7f542
DYT <DYT>
2020-03-23 09:21:28 +0800

    **

--- src/store/module/basemap.js
+++ src/store/module/basemap.js
@@ -1,28 +1,27 @@
-import Vue from 'vue'
-import ls from '@/utils/localStorage.js';
+import Vue from "vue";
+import ls from "@/utils/localStorage.js";
 Vue.use(ls.Storage, ls.options);
-import { currentBaseMapSource } from "@/mapTool/baseMapData.js"
+import { baseMapSourceList } from "@/config/map/baseMap.js";
 const basemap = {
-	state: {
-        /**
-         * 当前地图数据源 
-         * @type {{
-         * name:string,
-         * sourceKey:string,
-         * src:string,
-         * type:'baseMapRasterSource'|'baseMapGeojsonSource'
-         * }}
-         */
-		currentBaseMapSource: currentBaseMapSource
-	},
-	mutations: {
-
-	},
-	actions: {
-		setCurrenBaseMapSource({ state }, val) {
-			Vue.ls.set("currentBaseMapSource", val);
-			state.currentBaseMapSource = val;
-		}
-	}
-}
+  state: {
+    /**
+     * 当前地图数据源
+     * @type {{
+     * name:string,
+     * sourceKey:string,
+     * src:string,
+     * type:'baseMapRasterSource'|'baseMapGeojsonSource'
+     * }}
+     */
+    currentBaseMapSource:
+      Vue.ls.get("currentBaseMapSource") || baseMapSourceList[3]
+  },
+  mutations: {},
+  actions: {
+    setCurrenBaseMapSource({ state }, val) {
+      Vue.ls.set("currentBaseMapSource", val);
+      state.currentBaseMapSource = val;
+    }
+  }
+};
 export default basemap;
